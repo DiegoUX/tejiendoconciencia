721fd51b03f20d5f1feb5b9bf6d6d8d6644dc3fc
Diego Herrando <diegonzah@gmail.com>
2017-07-20 13:07:08 -0300

    uuultimos cambios

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@
 					<div class="row">
 						<div class="col-md-10 col-md-offset-1">
 							<div class="text-content text-center">
-								<h1><?php _e( 'Tejiendo un Blog', 'html5blank' ); ?></h1>
+								<h1 class="blog-title-tr"><?php dynamic_sidebar('blog-title'); ?></h1>
 							</div>
 						</div>
 					</div>
